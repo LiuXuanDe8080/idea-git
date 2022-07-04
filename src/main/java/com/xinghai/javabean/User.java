98db80949106025dcd59b75734cf65a63d062099
root <1776522367@qq.com>
2022-07-04 20:11:38 +0800

    第一次IDEA提交

---
+++ src/main/java/com/xinghai/javabean/User.java
@@ -0,0 +1,7 @@
+package com.xinghai.javabean;
+
+public class User {
+    public void uus(){
+        System.out.println("成功");
+    }
+}
---
+++ src/test/java/com/xinghai/Dome1.java
@@ -0,0 +1,10 @@
+package com.xinghai;
+
+import com.xinghai.javabean.User;
+
+public class Dome1 {
+    public static void main(String[] args) {
+        User user = new User();
+        user.uus();
+    }
+}
